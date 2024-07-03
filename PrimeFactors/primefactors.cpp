245cb7177ea9e79d56550c48362cffd4fb9d5d80
j1.woo <j1.woo@samsung.com>
2024-07-03 11:13:20 +0900

    tdd cycle 3

--- PrimeFactors/primefactors.cpp
+++ PrimeFactors/primefactors.cpp
@@ -1,8 +1,15 @@
 #include <vector>
-
+using namespace std;
 class PrimeFactor
 {
 public:
+	vector<int> of(int n)
+	{
+		vector<int> prime_factors = {};
+			if (n==2||n==3)
+				prime_factors.push_back(n);
+		return prime_factors;
+	}
 private:
 
 };
--- PrimeFactors_test/test.cpp
+++ PrimeFactors_test/test.cpp
@@ -1,7 +1,21 @@
 #include "pch.h"
 #include "../PrimeFactors/primefactors.cpp"
+#include <vector>
+using namespace std;
 
-TEST(PrimeFactors, TestName) {
-	EXPECT_EQ(1, 1);
-	EXPECT_TRUE(true);
+
+TEST(PrimeFactors, Of1) {
+	PrimeFactor prime_factor;
+	vector<int> expected = {};
+	EXPECT_EQ(expected, prime_factor.of(1));
+}
+TEST(PrimeFactors, Of2) {
+	PrimeFactor prime_factor;
+	vector<int> expected = {2};
+	EXPECT_EQ(expected, prime_factor.of(2));
+}
+TEST(PrimeFactors, Of3) {
+	PrimeFactor prime_factor;
+	vector<int> expected = {3};
+	EXPECT_EQ(expected, prime_factor.of(3));
 }
